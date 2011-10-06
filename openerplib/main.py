3500e1fb701b0615b1a6d4690ed16b006ef9412b
Xavier Morel <xmo@openerp.com>
2011-10-06 17:43:55 +0200

    [imp] shortcut search_read to not perform a read (and an additional RPC roundtrip) if search got no results

--- openerplib/main.py
+++ openerplib/main.py
@@ -394,6 +394,7 @@ class Model(object):
         :return: A list of dictionaries containing all the specified fields.
         """
         record_ids = self.search(domain or [], offset, limit or False, order or False, context or {})
+        if not record_ids: return []
         records = self.read(record_ids, fields or [], context or {})
         return records
 
